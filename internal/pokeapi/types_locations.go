7d7d85f9afae18a50fd2a521a9d0ece09564690b
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-12-08 14:30:20 -0300

    add pokemon and encounters types

--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -1,13 +1,13 @@
 package pokeapi
 
 type RespLocations struct {
-	Count		int		`json:"count"`
-	Next		*string	`json:"next"`
-	Previous	*string	`json:"previous"`
+	Count		int			`json:"count"`
+	Next		*string		`json:"next"`
+	Previous	*string		`json:"previous"`
 	Results		[]struct {
-		Name	string	`json:"name"`
-		URL		string	`json:"url"`
-	}`json:"results"`
+		Name	string		`json:"name"`
+		URL		string		`json:"url"`
+	}						`json:"results"`
 }
 
 type RespLocationPokemon struct {
@@ -19,4 +19,27 @@ type RespLocationPokemon struct {
 			Url		string			`json:"url"`
 		}							`json:"pokemon"`
 	}								`json:"pokemon_encounters"`
+}
+
+type RespPokemon struct {
+	Id						int		`json:"id"`
+	Name					string	`json:"name"`
+	BaseExperience			int		`json:"base_experience"`
+	Height					int		`json:"height"`
+	Weight					int		`json:"weight"`
+	Order					int		`json:"order"`
+	Stats					[]struct {
+		BaseStat			int		`json:"base_stat"`
+		Stat				struct {
+			Name			string	`json:"name"`
+			Url				string	`json:"url"`
+		}							`json:"stat"`
+	}								`json:"stats"`
+	Types					[]struct {
+		Slot				int		`json:"slot"`
+		Type				struct {
+			Name			string	`json:"name"`
+			Url				string	`json:"url"`
+		}							`json:"type"`
+	}								`json:"Types"`
 }
